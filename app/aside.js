9518eb9871721d304602b4f8375733b4935a2dcd
leandro-pessoa <119120060+leandro-pessoa@users.noreply.github.com>
2023-12-20 20:26:48 -0300

    adição de funcionalidades de scroll na section topics

--- app/aside.js
+++ app/aside.js
@@ -4,12 +4,18 @@ const menuBtn = document.querySelector('[data-menu-button]')
 const menuLateral = document.querySelector('[data-menu-lateral]')
 const menuLateralMenor = document.querySelector('[data-menu-lateral-menor]')
 const topicsContainer = document.querySelector('[data-topics]')
+let canaisContainer = document.querySelector('[data-canais]')
+let canaisOcultosContainer = document.querySelector('[data-canais-ocultos]')
+let canaisNumero = document.querySelector('[data-canais-numero]')
+const maisBtn = document.querySelector('[data-button-mais]')
+const menosBtn = document.querySelector('[data-button-menos]')
 
 // atribução do ano atual no copyright
 const data = new Date()
 const dataAtual = data.getFullYear()
 dataContainer.textContent = dataAtual
 
+// alterna entre os menus quando é alterado o tamanho da tela
 const resizeMenu = () => {
     if(document.body.clientWidth < 1281){
         menuLateral.style.display = 'none'
@@ -29,6 +35,7 @@ const resizeMenu = () => {
     }
 }
 
+// troca os menus ao clicar no button 'menu' do header
 const toggleMenu = () => {
     if(document.body.clientWidth >= 1281 && menuLateral.style.display == 'none' && menuLateralMenor.style.display == 'block'){
         menuLateral.style.display = 'block'
@@ -42,6 +49,51 @@ const toggleMenu = () => {
     }
 }
 
+// base de canais falsos
+const canais = [
+    {'nome': 'Canal 1'}, {'nome': 'Canal 2'}, {'nome': 'Canal 3'}, {'nome': 'Canal 4'}, {'nome': 'Canal 5'}, {'nome': 'Canal 6'}, {'nome': 'Canal 7'}, {'nome': 'Canal 8'}, {'nome': 'Canal 9'}, {'nome': 'Canal 10'}, {'nome': 'Canal 11'}, {'nome': 'Canal 12'}, {'nome': 'Canal 13'}
+]
+
+// insere os canais falsos na lista 'canaisContainer' até o limite de 7
+// quando passar do limite, irá inserir em 'canaisOcultosContainer'
+const inserirCanal = (nome) => {
+    const template = `
+        <li class="list__item">
+            <a href="#" class="list__item-link">
+                <span class="material-symbols-outlined">account_circle</span>${nome}
+            </a>
+        </li>
+    `
+
+    if(canaisContainer.childElementCount <= 6){
+        canaisContainer.innerHTML += template
+    }
+    else {
+        canaisOcultosContainer.innerHTML += template 
+    }
+
+    canaisNumero.textContent = canaisOcultosContainer.childElementCount
+    
+}
+
+// executa a função 'inserirCanal' repetidas vezes com o nome do canal como parâmetro
+canais.forEach((canal) => inserirCanal(canal.nome))
+
+// mostra os canais ocultos
+const mostrarMais = () => {
+    maisBtn.style.display = 'none'
+    canaisOcultosContainer.style.display = 'block'
+    menosBtn.style.display = 'flex'
+}
+// desaparece os canais ocultos
+const mostrarMenos = () => {
+    menosBtn.style.display = 'none'
+    canaisOcultosContainer.style.display = 'none'
+    maisBtn.style.display = 'flex'
+}
+
 // listeners de evento
 menuBtn.addEventListener('click', toggleMenu)
 window.addEventListener('resize', resizeMenu)
+maisBtn.addEventListener('click', mostrarMais)
+menosBtn.addEventListener('click', mostrarMenos)
--- app/topics.js
+++ app/topics.js
@@ -1,6 +1,14 @@
+// seleção de elementos HTML
+const topicsListContainer = document.querySelector('[data-topics]')
 const topicsListItem = document.querySelectorAll('.topics__list-item')
+const containerLeft = document.querySelector('.topics__button-container--left')
+const containerRight = document.querySelector('.topics__button-container--right')
+const leftBtn = document.querySelector('[data-button-left]')
+const rightBtn = document.querySelector('[data-button-right]')
 
+let pos = 0
 
+// ao clicar em um tópico, ele ficará com um estilo diferente e irá retirar esse estilo do anterior
 const activeItem = (e) => {
     topicsListItem.forEach((item) => {
         if(item.classList.contains('ativo')){
@@ -10,6 +18,48 @@ const activeItem = (e) => {
     e.target.classList.toggle('ativo')
 }
 
+const scrollLeft = () => {
+    
+    pos -= 100
+    console.log(pos)
+    topicsListContainer.scroll({
+        left: pos,
+        right: pos,
+        behavior: 'smooth'
+    })
+
+}
+const scrollRight = () => {
+    
+    pos += 100
+    console.log(pos)
+    topicsListContainer.scroll({
+        right: pos,
+        left: pos,
+        behavior: 'smooth'
+    })
+}
+
+const getScrollPosition = () => {
+    if(topicsListContainer.scrollLeft == 0) {
+        containerLeft.style.display = 'none'
+    }
+    else {
+        containerLeft.style.display = 'flex'
+    }
+
+    if(pos > topicsListContainer.scrollLeft) {
+        containerRight.style.display = 'none'
+    }
+    else {
+        containerRight.style.display = 'flex'
+    }
+}
+
+// listeners de evento
 topicsListItem.forEach((item) => {
     item.addEventListener('click', activeItem)
 })
+topicsListContainer.addEventListener('scroll', getScrollPosition)
+leftBtn.addEventListener('click', scrollLeft)
+rightBtn.addEventListener('click', scrollRight)
